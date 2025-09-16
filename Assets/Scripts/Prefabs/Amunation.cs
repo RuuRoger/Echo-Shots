a60ed14c27ae96371a1265547ace07262bfb1772
Roger Lozano <lozanofierroruu@gmail.com>
2025-09-16 22:06:36 +0200

    feat: scrit to amunation. Notify when is touched to full bullets

---
+++ Assets/Scripts/Prefabs/Amunation.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Prefabs
+{
+    public class Amunation : MonoBehaviour
+    {
+        //Events
+        public event Action OnGetAmunation;
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                OnGetAmunation?.Invoke();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
