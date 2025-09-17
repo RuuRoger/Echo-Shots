a7f083da99489a00d0ad57de55acaf743eb7950e
Roger Lozano <lozanofierroruu@gmail.com>
2025-09-17 16:45:53 +0200

    feat: subscribe to liveshandeler when player is hitted

--- Assets/Scripts/Gun/WeaponShoot.cs
+++ Assets/Scripts/Gun/WeaponShoot.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Assets.Scripts.Player;
+using Assets.Scripts.Prefabs;
 
 namespace Assets.Scripts.Gun
 {
@@ -14,6 +15,7 @@ namespace Assets.Scripts.Gun
         private WeaponFlipController m_weaponFlipController;
         private Transform m_gunBarrel;
         private PlayerMovement m_player;
+        private PlayerManager m_playerManager;
         private bool m_isFlipped;
 
         private void Awake()
@@ -22,6 +24,7 @@ namespace Assets.Scripts.Gun
             m_weaponFlipController = GetComponent<WeaponFlipController>();
             m_gunBarrel = transform.GetChild(0);
             m_player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+            m_playerManager = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
         }
 
         private void OnEnable()
@@ -49,6 +52,13 @@ namespace Assets.Scripts.Gun
             GameObject bullet = GameObject.Instantiate(m_bulletPrefab, m_gunBarrel.position, m_gunBarrel.rotation);
             bullet.transform.position += new Vector3(0.2f * directionMultiplier, 0f, 0f);
             bullet.GetComponent<Rigidbody2D>().AddForce(m_gunBarrel.right * directionMultiplier * m_speedBullet, ForceMode2D.Impulse);
+            
+            Bullet bulletScript = bullet.GetComponent<Bullet>();
+            if (bulletScript != null)
+            {
+                bulletScript.OnHitPlayer += m_playerManager.LivesHandler;
+            }
+            
             Destroy(bullet, 10f);
         }
     }
